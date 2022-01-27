76ea819827c71b7a0c7ae391ecb1203427c2ae19
Azshian <aliceralde@egi.utah.edu>
2022-01-26 17:28:54 -0700

    DS3231 Guide

--- DS3231/DS3231.ino
+++ DS3231/DS3231.ino
@@ -1,9 +1,26 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <RTClib.h>
+RTC_DS3231 rtc;
+DateTime dt;
 
+void setup() {
+  Serial.begin(9600);
+  RTCBegin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  dt = rtc.now(); 
+  Serial.println(dt.unixtime());
+  while (rtc.now().unixtime() == dt.unixtime());  
+}
 
+void RTCBegin() {
+  bool success = false;
+  while (success == false) {
+    if(rtc.begin())
+      success = true;
+    else
+      Serial.println("RTC Failed");
+    delay(1000);
+  }
+  Serial.println("RTC Operational");
 }
